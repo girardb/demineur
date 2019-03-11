955669da328abe0ceaadc423a648c7f887f47ed0
Benjamin Girard <benjamin.girard.protic@gmail.com>
2019-03-10 23:30:55 -0400

    Je peux jouer dans le terminal. Le code est laid though.

---
+++ niveau2.py
@@ -0,0 +1,11 @@
+from niveau1.py import Minesweeper
+
+# Generate random grids
+
+# Hide non-revealed tiles
+
+# Reveal "touching" '0' tiles when one is hit
+
+# Change '0' tiles to blank spaces
+
+# Make it playing from command-line by with inputs '(x, y)' # Use list(str.strip('(),'))
